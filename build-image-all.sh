92b5205d0e4c4ee79dc3c8fda93be6c549eb9f10
Pedro Cheroto <pedro.cheroto@gmail.com>
2020-05-19 19:40:36 -0400

    Added build all images script.

---
+++ build-image-all.sh
@@ -0,0 +1,14 @@
+STARTINGPATH=${PWD}
+REPOSITORIES=(
+	favorite-restaurants
+	restaurantsAPI
+)
+for REPOSITORY in ${REPOSITORIES[*]}
+do
+	echo ===========================================
+	echo Building local docker image: $REPOSITORY
+	echo ===========================================
+	cd $REPOSITORY
+	docker build -t ${REPOSITORY,,} .
+	cd $STARTINGPATH
+done
